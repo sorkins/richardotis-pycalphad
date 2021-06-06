a2718d9c28d66fd254dd32bedea03969535dd644
Richard Otis <rotis@jpl.nasa.gov>
2021-06-05 17:47:58 -0700

    Landing page bump

--- docs/conf.py
+++ docs/conf.py
@@ -156,7 +156,9 @@ html_static_path = ['_static']
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-#html_additional_pages = {}
+html_additional_pages = {
+    "index": "landing.html"
+}
 
 # If false, no module index is generated.
 #html_domain_indices = True
